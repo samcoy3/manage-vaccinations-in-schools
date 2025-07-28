d924e226dc73e5e2677982b69d4d0c10b8997e03
Thomas Leese <thomas@leese.io>
2025-07-28 19:52:13 +0100

    Add workgroup column
    This adds a new column to the team model which represents the
    "workgroup" identifier of the team. This will be used when
    authenticating via CIS2 to select which teams the user has access to.
    
    Jira-Issue: MAV-1280

--- db/schema.rb
+++ db/schema.rb
@@ -766,8 +766,10 @@ ActiveRecord::Schema[8.0].define(version: 2025_07_29_115701) do
     t.string "privacy_notice_url", null: false
     t.string "phone_instructions"
     t.bigint "organisation_id", null: false
+    t.string "workgroup", null: false
     t.index ["name"], name: "index_teams_on_name", unique: true
     t.index ["organisation_id"], name: "index_teams_on_organisation_id"
+    t.index ["workgroup"], name: "index_teams_on_workgroup", unique: true
   end
 
   create_table "teams_users", id: false, force: :cascade do |t|
