d85315fa7aafab84d60dabe3f5dde59f28e73e18
Thomas Leese <thomas@leese.io>
2025-07-29 19:05:02 +0100

    Show eligible programmes on patient page
    This adds a new section to the patient page showing which programmes the
    patient is eligible for at which time. In the prototype, this card is
    supposed to replace the cohorts card, but since we haven't built the
    archive functionality that's designed to replace "Remove from cohort"
    I've left it in place.
    
    Jira-Issue: MAV-1516

--- app/models/patient_session.rb
+++ app/models/patient_session.rb
@@ -89,8 +89,6 @@ class PatientSession < ApplicationRecord
 
   scope :appear_in_programmes,
         ->(programmes) do
-          age_children_start_school = 5
-
           # Is the patient eligible for any of those programmes by year group?
           location_programme_year_groups =
             Location::ProgrammeYearGroup
@@ -99,7 +97,7 @@ class PatientSession < ApplicationRecord
               .where(
                 "year_group = sessions.academic_year " \
                   "- patients.birth_academic_year " \
-                  "- #{age_children_start_school}"
+                  "- #{Integer::AGE_CHILDREN_START_SCHOOL}"
               )
 
           # Are any of the programmes administered in the session?
