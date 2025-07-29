6a7d94de5fa2c2c2205399fa32cda28801a9b236
Thomas Leese <thomas@leese.io>
2025-07-29 17:51:40 +0100

    Add option to not send consent refusal to parents
    This adds a question when recording verbal consent, and where the parent
    has refused for personal reasons, allowing the nurses to not send a
    confirmation to this parent.
    
    This will be used in the preparation period for the next academic year
    where nurses will record refusal for parents they know will refuse
    consent and wouldn't like to receive confirmation.
    
    Jira-Issue: MAV-1586

--- app/controllers/draft_consents_controller.rb
+++ app/controllers/draft_consents_controller.rb
@@ -74,7 +74,9 @@ class DraftConsentsController < ApplicationController
 
     set_patient_session # reload with new statuses
 
-    send_triage_confirmation(@patient_session, @programme, @consent)
+    if @draft_consent.send_confirmation?
+      send_triage_confirmation(@patient_session, @programme, @consent)
+    end
 
     heading_link_href =
       session_patient_programme_path(@session, @patient, @programme)
@@ -118,6 +120,7 @@ class DraftConsentsController < ApplicationController
     permitted_attributes = {
       agree: %i[response injection_alternative],
       notes: %i[notes],
+      notify_parent_refusal: %i[notify_parent_refusal],
       notify_parents: %i[notify_parents],
       parent_details: %i[
         parent_email
