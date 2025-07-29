1df7e35538fb59d0ac603d7c4904df4d5cd21944
Sam Coy <sam.coy@tngtech.com>
2025-07-29 14:01:01 +0100

    Log request details for PII accesses in timeline
    - Log fields accessed during a request which accessed PII
    - Log whether audits were enabled

--- app/controllers/inspect/timeline/patients_controller.rb
+++ app/controllers/inspect/timeline/patients_controller.rb
@@ -4,7 +4,7 @@ class Inspect::Timeline::PatientsController < ApplicationController
   skip_after_action :verify_policy_scoped
   before_action :set_patient
   before_action :set_show_pii
-  before_action :record_access_log_entry
+  after_action :record_access_log_entry
 
   layout "full"
 
@@ -146,14 +146,14 @@ class Inspect::Timeline::PatientsController < ApplicationController
   end
 
   def pii_accessed?
-    detail_config = build_details_config
-
-    detail_config.any? do |event_type, selected_fields|
-      pii_fields =
-        TimelineRecords::AVAILABLE_DETAILS_CONFIG_PII[event_type] || []
-      (selected_fields & pii_fields).any?
-    end
-  end
+    return false unless @show_pii
+        detail_config = build_details_config
+        detail_config.any? do |event_type, selected_fields|
+          pii_fields =
+            TimelineRecords::AVAILABLE_DETAILS_CONFIG_PII[event_type] || []
+          (selected_fields & pii_fields).any?
+        end
+      end
 
   def audit_pii_accessed?
     true if @show_pii && params[:event_names].include?("audits")
@@ -161,10 +161,12 @@ class Inspect::Timeline::PatientsController < ApplicationController
 
   def record_access_log_entry
     if pii_accessed? || audit_pii_accessed?
-      @patient.access_log_entries.create!(
-        user: current_user,
-        controller: "timeline",
-        action: "show_pii"
+      details_accessed = build_details_config.reverse_merge(params[:event_names].map { |key| [key.to_sym, []] }.to_h)
+          @patient.access_log_entries.create!(
+            user: current_user,
+            controller: "timeline",
+            action: "show_pii",
+            request_details: details_accessed
       )
     end
   end
